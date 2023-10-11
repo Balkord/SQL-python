7973124cbb42dfb462e72b9935f8a3044a643bdf
Balkord <verdaguer.lppdg@gmail>
2023-10-11 09:48:40 +0200

    Ajout de texte explication

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ cursor = connection.cursor()
 
 new_user = (cursor.lastrowid, "Toto", 12)
 cursor.execute('INSERT INTO t_users VALUES(?,?,?)', new_user)
+
+#Envoie les informations précédente
 connection.commit()
+
 print("Nouvelle utilisateur ajouter ! ")
+
+#fermer la requête de la base de donnée
 connection.close()
 
