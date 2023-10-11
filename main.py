d069c45aa02d25040b85601c53b4757fd238ab59
Balkord <verdaguer.lppdg@gmail>
2023-10-11 10:09:05 +0200

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sqlite3
 connection = sqlite3.connect("base.db")
 cursor = connection.cursor()
 
-new_user = (cursor.lastrowid, "Toto", 12)
+new_user = (cursor.lastrowid, "Jean", 20)
 cursor.execute('INSERT INTO t_users VALUES(?,?,?)', new_user)
 
 #Envoie les informations précédente
@@ -16,3 +16,8 @@ print("Nouvelle utilisateur ajouter ! ")
 #fermer la requête de la base de donnée
 connection.close()
 
+#On peux pas créer plusieur user a entré dans la base de donné.
+
+#Voir comment intégré plusieur USER en même temp
+
+#Voir la finalité de la base de donnée et comment l'utiliser pour switch sur SQL
